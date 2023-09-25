8d920c170cf64e70562da21c6ff47b76fd3708cf
Felipe Lecot <47688222+FelipeLecot@users.noreply.github.com>
2023-09-25 10:08:32 -0300

    Added in-out functionality

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,29 @@
-extern crate cpal;
+use std::io;
 
 fn main() {
-    let input_format = cpal::Format {
-        channels: 1,
-        sample_rate: cpal::SampleRate(44100),
-        data_type: cpal::SampleFormat::I16,
-    };
+    println!("Welcome to the Rust CLI Service!");
+    loop {
+        println!("Enter a string (or 'exit' to quit):");
 
-    let event_loop = cpal::EventLoop::new();
-    let input_device = cpal::default_input_device().expect("No input device available");
+        let mut input = String::new();
 
-    let stream_id = event_loop.build_input_stream(&input_device, &input_format)?;
-    event_loop.play_stream(stream_id)?;
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
 
-    event_loop.run(move |_, data| {
-        if let cpal::StreamData::Input { buffer, .. } = data {
-            // Process audio samples here
-            // Call the speech-to-text function with 'buffer'
+        let input = input.trim();
+
+        if input.to_lowercase() == "exit" {
+            break;
         }
-    });
+
+        let transformed = transform_string(input);
+        println!("Transformed result: {}", transformed);
+    }
 }
 
-extern crate vosk;
-
-fn recognize_speech(audio_samples: &[i16]) -> Result<String, Box<dyn std::error::Error>> {
-    let model = vosk::Model::new("path/to/model")?;
-    let mut recognizer = model.new_recognizer(16000.0)?;
-    recognizer.accept_waveform(audio_samples, 16000)?;
-    let result = recognizer.final_result();
-    Ok(result.text)
-}
-
-extern crate reqwest;
-
-fn send_to_api(text: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let url = "https://api.example.com/stt";
-    let client = reqwest::blocking::Client::new();
-    let response = client.post(url).body(text).send()?;
-    // Handle the response here
-    Ok(())
+fn transform_string(input: &str) -> String {
+    // Replace this with your custom transformation logic
+    // For example, you can make it uppercase:
+    input.to_uppercase()
 }
